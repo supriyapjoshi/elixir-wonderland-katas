ec581454aadabc3e364740361e7d027a890ae5b3
supriya <supriyapjoshi@gmail.com>
2015-12-19 14:46:43 +1100

    Refactored to remove a bit of duplication

--- alphabet-cipher/lib/cipher.ex
+++ alphabet-cipher/lib/cipher.ex
@@ -1,7 +1,15 @@
 defmodule Cipher do
-  def cipher(input, code), do: _cipher([],to_char_list(input), List.duplicate(to_char_list(code),String.length(input))|> List.flatten) |> Enum.reverse |> to_string
-  
-  defp _cipher(list, [input_head | input_tail], [code_head | code_tail]) when (code_head - ?a)+input_head <= ?z, do: _cipher([(code_head - ?a) + input_head | list], input_tail, code_tail)
-  defp _cipher(list, [input_head | input_tail], [code_head | code_tail]) when (code_head - ?a)+input_head > ?z, do: _cipher([(code_head - ?a) + input_head - 26 | list], input_tail, code_tail)
+  def cipher(input, code) do
+    _cipher([],to_char_list(input), List.duplicate(to_char_list(code),String.length(input))|> List.flatten) |> Enum.reverse |> to_string
+  end
+  defp _cipher(list, [input_head | input_tail], [code_head | code_tail]) do
+    val = cal(code_head, input_head)
+    cond do
+      val <= ?z -> _cipher([val | list], input_tail, code_tail)
+      val > ?z  ->  _cipher([val- 26 | list], input_tail, code_tail)
+    end
+  end
   defp _cipher(list, [], _), do: list
+  defp cal(code, input), do: (code - ?a) + input
+
 end
